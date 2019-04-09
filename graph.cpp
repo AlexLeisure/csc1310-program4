8bd748467f13faa9fb3bc8e72049a3c0a39ea9fa
Alex <alex.leisure125@gmail.com>
2019-04-09 13:46:14 -0500

    Added basic structure to Graph.cpp

---
+++ graph.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "GraphList.h"
+#include "GraphMatrix.h"
+
+using namespace std;
+
+int main(){
+    cout << "This is a test message.\n";
+    return 0;
+}
