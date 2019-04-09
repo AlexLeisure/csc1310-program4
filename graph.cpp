851e073e001e00c1149bd3e1f9db223db848d91c
Alex <alex.leisure125@gmail.com>
2019-04-09 14:20:49 -0500

    Added test for GraphMatrix

--- graph.cpp
+++ graph.cpp
@@ -1,12 +1,24 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include "GraphList.h"
+//#include "GraphList.h"
 #include "GraphMatrix.h"
 
 using namespace std;
 
 int main(){
-    cout << "This is a test message.\n";
+    const int numVertex = 4;
+    GraphMatrix* graph = new GraphMatrix(numVertex);
+    graph->addEdge(0, 1);
+    graph->addEdge(1, 2);
+    graph->addEdge(2, 3);
+    graph->addEdge(0, 3);
+
+    graph->print();
+
+    cout << (graph->hasEdge(0, 1)) ? "There is an edge 0->1\n" : "There is not an edge 0->1\n";
+    cout << (graph->hasEdge(2, 1)) ? "There is an edge 2->1\n" : "There is not an edge 2->1\n";
+
+    delete graph;
     return 0;
 }
