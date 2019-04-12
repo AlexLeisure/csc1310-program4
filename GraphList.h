0608dfcaca85a177dcb31ef9435239e5d3c60d58
Alex <alex.leisure125@gmail.com>
2019-04-12 14:37:30 -0500

    Took GraphList.h file from GraphList branch

---
+++ GraphList.h
@@ -0,0 +1,70 @@
+#ifndef GRAPHLIST_H
+#define GRAPHLIST_H
+
+#include<iostream>
+#include<string>
+
+class GraphList{
+private:
+    struct Node{
+        int value;
+        Node* next;
+    };
+    Node** adjList;
+    int numVertex;
+    int numEdge;
+
+public:
+    GraphList(int numVertex){
+        this->numVertex = numVertex;
+        numEdge = 0;
+        adjList = new Node*[numVertex];
+        for(int i=0; i<numVertex; i++){
+            adjList[i] = new Node;
+            adjList[i]->value = i;
+            adjList[i]->next = nullptr;
+        }
+    }
+    ~GraphList(){
+        Node* tempNode;
+        for(int i=0; i<numVertex; i++){
+            Node* node = adjList[i];
+            while(node){
+                tempNode = node->next;
+                delete node;
+                node = tempNode;
+            }
+        }
+        delete [] adjList;
+    }
+    void addEdge(int vOne, int vTwo){
+        if(vOne >= numVertex || vTwo >= numVertex){
+            std::cout << "Index out of bounds...\n";
+            return;
+        }
+        Node* node = adjList[vOne];
+        while(node->next){
+            node = node->next;
+        }
+        Node* newNode = new Node;
+        newNode->value = vTwo;
+        newNode->next = nullptr;
+        node->next = newNode;
+        numEdge++;
+    }
+    void print(){
+        std::cout << "List\n";
+        string arrow = "--->";
+        for(int i=0; i<numVertex; i++){
+            Node* node = adjList[i];
+            std::cout << node->value << arrow;
+            while(node->next){
+                node = node->next;
+                std::cout << node->value << arrow;
+            }
+            std::cout << "NULL\n";
+        }
+    }
+};
+
+#endif
